0b5810a33e0f96620933cf56948833e78759ec2b
codewithrt <rtembhurne2802@gmail.com>
2023-12-10 13:29:12 +0530

    modified fox

--- 3Dmain/vite-project/src/models/Fox.jsx
+++ 3Dmain/vite-project/src/models/Fox.jsx
@@ -25,37 +25,48 @@ const Fox = ({currentAnimation,...props})=> {
   return (
     <group ref={group} {...props} dispose={null}>
       <group name="Sketchfab_Scene">
-        <primitive object={nodes.GLTF_created_0_rootJoint} />
-        <skinnedMesh
-          name="Object_7"
-          geometry={nodes.Object_7.geometry}
-          material={materials.PaletteMaterial001}
-          skeleton={nodes.Object_7.skeleton}
-        />
-        <skinnedMesh
-          name="Object_8"
-          geometry={nodes.Object_8.geometry}
-          material={materials.PaletteMaterial001}
-          skeleton={nodes.Object_8.skeleton}
-        />
-        <skinnedMesh
-          name="Object_9"
-          geometry={nodes.Object_9.geometry}
-          material={materials.PaletteMaterial001}
-          skeleton={nodes.Object_9.skeleton}
-        />
-        <skinnedMesh
-          name="Object_10"
-          geometry={nodes.Object_10.geometry}
-          material={materials.PaletteMaterial001}
-          skeleton={nodes.Object_10.skeleton}
-        />
-        <skinnedMesh
-          name="Object_11"
-          geometry={nodes.Object_11.geometry}
-          material={materials.PaletteMaterial001}
-          skeleton={nodes.Object_11.skeleton}
-        />
+        <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]}>
+          <group name="root">
+            <group name="GLTF_SceneRootNode" rotation={[Math.PI / 2, 0, 0]}>
+              <group name="Armature002_27">
+                <group name="GLTF_created_0">
+                  <primitive object={nodes.GLTF_created_0_rootJoint} />
+                  <skinnedMesh
+                    name="Object_7"
+                    geometry={nodes.Object_7.geometry}
+                    material={materials["Material.012"]}
+                    skeleton={nodes.Object_7.skeleton}
+                  />
+                  <skinnedMesh
+                    name="Object_8"
+                    geometry={nodes.Object_8.geometry}
+                    material={materials["Material.013"]}
+                    skeleton={nodes.Object_8.skeleton}
+                  />
+                  <skinnedMesh
+                    name="Object_9"
+                    geometry={nodes.Object_9.geometry}
+                    material={materials["Material.014"]}
+                    skeleton={nodes.Object_9.skeleton}
+                  />
+                  <skinnedMesh
+                    name="Object_10"
+                    geometry={nodes.Object_10.geometry}
+                    material={materials["Material.016"]}
+                    skeleton={nodes.Object_10.skeleton}
+                  />
+                  <skinnedMesh
+                    name="Object_11"
+                    geometry={nodes.Object_11.geometry}
+                    material={materials["Material.017"]}
+                    skeleton={nodes.Object_11.skeleton}
+                  />
+                  <group name="Cube002_26" />
+                </group>
+              </group>
+            </group>
+          </group>
+        </group>
       </group>
     </group>
   );
